e3fec76caa249828bda71c42c004633e7a02e786
Narotam-Mishra <narotam21jan@gmail.com>
2025-01-12 22:04:02 +0530

    flight-booking-app_setup_p2_1.2

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,5 @@
 
 
 module.exports = {
-    
+    InfoController: require('./info-controller')
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 
 const express = require('express');
-const { PORT } = require('./config');
-
+const { ServerConfig } = require('./config');
+const apiRoutes = require('./routes');
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Successfully started the server on PORT: ${PORT}`);
+app.use('/api', apiRoutes);
+
+app.listen(ServerConfig.PORT, () => {
+    console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
 })
