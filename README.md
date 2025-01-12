54f7868b20e6982ba54aad1014165246d641f2ad
Narotam-Mishra <narotam21jan@gmail.com>
2025-01-13 00:00:43 +0530

    flight-booking-app_crud-with-db-setup_p2_1.5

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+- [Sequelize](https://sequelize.org/)
+- [Sequelize Setup](https://sequelize.org/docs/v6/getting-started/)
--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 const app = express();
 
+// parse incoming body request (json)
+app.use(express.json());
+
+// parse incoming body request (urlencoded data)
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
