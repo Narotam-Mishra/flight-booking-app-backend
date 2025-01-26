fc6c90c6ecaa66cee344ae2b39bec5f9ec9be208
Narotam-Mishra <narotam21jan@gmail.com>
2025-01-27 00:16:28 +0530

    flight-booking-app_added_one-to-many_example_1.26

--- src/index.js
+++ src/index.js
@@ -13,12 +13,5 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async() => {
-    console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
-
-    const { City, Airport } = require('./models');
-    const bangalore = await City.findByPk(1);
-    console.log("City:", bangalore);
-
-    const kmpAirport = await bangalore.createAirport({ name: 'Kempgowda Airport', airportCode: 'BLR'});
-    console.log("Airport:", kmpAirport);  
+    console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);  
 })
