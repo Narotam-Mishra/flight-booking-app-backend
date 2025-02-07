50fdb501c18ec889e807204e7ff0379176884d31
Narotam-Mishra <narotam21jan@gmail.com>
2025-02-07 23:20:21 +0530

    flight-booking-app_added_eager_loading_for_joins_p2_1.36

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,6 +1,7 @@
 
 const CrudRepository = require('./crud-repository');
-const { Flight, Airplane } = require('../models');
+const { Flight, Airplane, Airport } = require('../models');
+const { Sequelize } = require('sequelize');
 
 class FlightRepository extends CrudRepository {
     constructor(){
@@ -11,10 +12,20 @@ class FlightRepository extends CrudRepository {
         const response = await Flight.findAll({
             where: filter,
             order: sort,
-            include: {
-                model: Airplane,
-                required: true,
-            }
+            include: [
+                {
+                    model: Airplane,
+                    required: true,
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    // as: "DepartureAirport",
+                    on: {
+                        col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("Airport.airportCode")),
+                    },
+                }
+            ]
         })
         // console.log("Filter Response:", response);
         return response;
