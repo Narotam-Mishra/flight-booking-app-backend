7c98dde6d39315af4f8fd932662312bebb0461aa
Narotam-Mishra <narotam21jan@gmail.com>
2025-02-11 23:54:38 +0530

    flight-booking-app_added_seed_for_seats_1.40

---
+++ src/seeders/20250211174716-add-seats.js
@@ -0,0 +1,126 @@
+'use strict';
+
+const { Enums } = require('../utils/common');
+const { BUSINESS, PREMIUM_ECONOMY, ECONOMY, FIRST_CLASS } = Enums.SEAT_TYPE;
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert('Seats', [
+      {
+        airplaneId: 1,
+        row: 1,
+        col: 'A',
+        seatType: ECONOMY,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        airplaneId: 1,
+        row: 2,
+        col: 'B',
+        seatType: ECONOMY,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        airplaneId: 1,
+        row: 3,
+        col: 'C',
+        seatType: ECONOMY,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        airplaneId: 1,
+        row: 4,
+        col: 'D',
+        seatType: ECONOMY,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        airplaneId: 1,
+        row: 5,
+        col: 'E',
+        seatType: BUSINESS,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        airplaneId: 1,
+        row: 6,
+        col: 'F',
+        seatType: PREMIUM_ECONOMY,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        airplaneId: 2,
+        row: 1,
+        col: 'A',
+        seatType: ECONOMY,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        airplaneId: 2,
+        row: 2,
+        col: 'B',
+        seatType: ECONOMY,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        airplaneId: 2,
+        row: 3,
+        col: 'C',
+        seatType: ECONOMY,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        airplaneId: 2,
+        row: 4,
+        col: 'D',
+        seatType: ECONOMY,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        airplaneId: 2,
+        row: 5,
+        col: 'E',
+        seatType: FIRST_CLASS,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        airplaneId: 2,
+        row: 6,
+        col: 'F',
+        seatType: BUSINESS,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
